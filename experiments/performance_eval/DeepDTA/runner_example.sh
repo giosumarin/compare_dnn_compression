629d74692a8fb63df68028d4ee49b154b2504ad6
giosumarin <giosumarin@yahoo.com>
2022-03-17 11:00:23 +0100

    correct paths

--- experiments/performance_eval/DeepDTA/runner_example.sh
+++ experiments/performance_eval/DeepDTA/runner_example.sh
@@ -6,23 +6,23 @@
 # we use iMap for convolutional layer and HAM for dense layer.
 # Results of compression ratio will be printed on terminal and will be saved in txt format 
 # at path experiments/space_performance/results/.
-python compression.py --compression uECSQ --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 64 --clustercnn 64
+python compression.py --compression uECSQ --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 64 --clustercnn 64 --epochs 10
 cd ../../space_performance
-python uws_testing_space.py -t also_cnn -d ../performance_eval/DeepDTA/deepDTA_davis/uECQS/ -m ../performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 -s davis -q 0
+python uws_testing_space.py -t also_cnn -d ../performance_eval/DeepDTA/deepDTA_davis/uECSQ/ -m ../performance_eval/DeepDTA/original_nets/deepDTA_davis.h5 -s davis -q 0
 
-cd ../../performance_eval
+cd ../performance_eval/DeepDTA/
 
 
 # This example applies pruning and uCWS quantization to the DeepDTA model trained on the Kiba dataset.
-# Pruning is only applied to fully connected layers (percentage 60%), while clustering is applied to 
-# fully connected (32 clusters) and convolutional (128 clusters) layers.
+# Pruning is only applied to fully connected layers (percentage 50%), while clustering is applied to 
+# fully connected (32 clusters) and convolutional (64 clusters) layers.
 # After compression we calculate space ratio compression (psi in paper) as compressed_space/uncompressed_space,
 # we use iMap for convolutional layer, for dense layer we calculate space with HAM and sHAM.
 # Results of compression ratio will be printed on terminal and will be saved in txt format 
 # at path experiments/space_performance/results/.
 
-python compression.py --compression pruCWS --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clustercnn 64 --clusterfc 64 --prfc 60
+python compression.py --compression pruCWS --net original_nets/deepDTA_kiba.h5 --dataset KIBA --clustercnn 64 --clusterfc 32 --prfc 50 --epochs 10
 cd ../../space_performance
 python uws_testing_space.py -t also_cnn -d ../performance_eval/DeepDTA/deepDTA_kiba/pruCWS/ -m ../performance_eval/DeepDTA/original_nets/deepDTA_kiba.h5 -s kiba -q 0
 
-cd ../../performance_eval
+cd ../performance_eval/DeepDTA/
