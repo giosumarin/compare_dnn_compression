9ad8ff4dd2c264a7fd8f47c26c5134f2f924d339
Marco Frasca <frasca@di.unimi.it>
2022-03-18 12:15:22 +0100

    Update runner_example.sh

--- experiments/performance_eval/DeepDTA/runner_example.sh
+++ experiments/performance_eval/DeepDTA/runner_example.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # This example applies uECQS quantization to the DeepDTA model trained on the Davis dataset.
-# Clustering is applied to CNN (64 clusters) and FC (64 clusters) layer. 
-# After compression we calculate space ratio compression (psi in paper) as compressed_space/uncompressed_space,
-# we use iMap for convolutional layer and HAM for dense layer.
+# Clustering is applied to convolutional (64 clusters) and FC (64 clusters) layers. 
+# After compression we calculate space occupancy (psi in paper), using
+# Index Map for convolutional layers and HAM for dense layers.
 # Results of compression ratio will be printed on terminal and will be saved in txt format 
 # at path experiments/space_performance/results/.
 python compression.py --compression uECSQ --net original_nets/deepDTA_davis.h5 --dataset DAVIS --clusterfc 64 --clustercnn 64 --epochs 10
@@ -16,8 +16,8 @@ cd ../performance_eval/DeepDTA/
 # This example applies pruning and uCWS quantization to the DeepDTA model trained on the Kiba dataset.
 # Pruning is only applied to fully connected layers (percentage 50%), while clustering is applied to 
 # fully connected (32 clusters) and convolutional (64 clusters) layers.
-# After compression we calculate space ratio compression (psi in paper) as compressed_space/uncompressed_space,
-# we use iMap for convolutional layer, for dense layer we calculate space with HAM and sHAM.
+# After compression we calculate space occupancy (psi in paper), using
+# Index Map for convolutional layers and HAM or sHAM for dense layers.
 # Results of compression ratio will be printed on terminal and will be saved in txt format 
 # at path experiments/space_performance/results/.
 
